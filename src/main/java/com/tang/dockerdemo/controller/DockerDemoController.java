ea531a224339e7d0ec552918ee5be895ecf1bce0
Tang <he_guitang@163.com>
2019-09-03 09:56:20 +0800

    DockerDemo访问示例完善

--- src/main/java/com/tang/dockerdemo/controller/DockerDemoController.java
+++ src/main/java/com/tang/dockerdemo/controller/DockerDemoController.java
@@ -1,7 +1,9 @@
 package com.tang.dockerdemo.controller;
 
+import com.tang.dockerdemo.service.DockerDemoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerDemoController {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private DockerDemoService dockerDemoService;
 
     @RequestMapping("/demo")
     @ResponseBody
